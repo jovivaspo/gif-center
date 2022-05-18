7ccf9615c4c478a3efcdda64e2fb7b219e498c2a
Vivas Pocostales Jorge <jovivaspo@gmail.com>
2022-05-18 22:38:06 +0200

    img responsive

--- src/pages/GifPage.js
+++ src/pages/GifPage.js
@@ -7,31 +7,42 @@ import { SiTelegram } from 'react-icons/si'
 
 const GifOrSticker = () => {
     const [content, setContent] = useState(null)
+    const [tag, setTag] = useState(null)
     const { id } = useParams()
     useEffect(() => {
-        const uri = endpoints.API_URL_GIFS + `/${id}${endpoints.MY_KEY}`
-        const loadContent = () => {
-            fetchData(uri)
-                .then(res => setContent(res.data))
+        const uri_gif = endpoints.API_URL_GIFS + `/${id}${endpoints.MY_KEY}`
+       
+        const loadContent = async () => {
+           fetchData(uri_gif)
+           .then(res=>{
+               setContent(res.data)
+               const uri_term = endpoints.URL_TAG_TERM +  `/${res.data.title}${endpoints.MY_KEY}`
+               fetchData(uri_term)
+               .then(res=>setTag(res.data))
+            })
         }
-        loadContent()
-    }, [])
 
-    console.log(content)
+        loadContent()
+
+    }, [])
 
     return (
         <div className='container'>
             <div className='container_img'>
-                <img className="img_main" src={content?.images.downsized_medium.url} alt={content?.title} />
+                <img className="img_main"
+                    src={content?.images.downsized_medium.url}
+                    alt={content?.title} />
             </div>
             <div className='container_info'>
-                <img className="img_info" src={content?.images.fixed_width_small.webp} />
+                <img className="img_info"
+                    src={content?.images.fixed_width_small.webp}
+                />
                 <div className="text">
                     <p>{content?.title}</p>
                     <p>{content?.user.display_name}</p>
                 </div>
                 <div className='container_icon'>
-                    <SiTelegram size={32}/>
+                    <SiTelegram size={32} />
                 </div>
             </div>
             <div>grid with related</div>
--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -5,13 +5,15 @@ const CATEGORY_TREDING=`${API_URL_GIFS}/trending`
 const N_OBJECT='30'
 const URL_TREDING_GIFS=`${CATEGORY_TREDING}?api_key=${API_KEY}&limit=${30}&rating=g`
 
-const MY_KEY = `&api_key=${API_KEY}`
+const MY_KEY = `?api_key=${API_KEY}`
 const URL_SERCH_GIFS=`${API_URL_GIFS}/search?q=`
 
 const API_URL_STICKERS="https://api.giphy.com/v1/stickers"
 const URL_SERCH_STICKERS=`${API_URL_STICKERS}/search?q=`
 const CATEGORY_TREDING_STICKERS=`${API_URL_STICKERS}/trending`
 const URL_TREDING_STICKERS=`${CATEGORY_TREDING_STICKERS}?api_key=${API_KEY}&limit=${30}&rating=g`
+
+const URL_TAG_TERM = "https://api.giphy.com/v1/tags/related"
 
 export const endpoints = {
     API_KEY,
@@ -25,5 +27,5 @@ export const endpoints = {
     URL_TREDING_STICKERS,
     URL_SERCH_STICKERS,
     MY_KEY,
-   
+    URL_TAG_TERM
 }
