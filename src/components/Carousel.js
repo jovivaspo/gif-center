d693351e65573d99a83f738b20f6c90335033891
Vivas Pocostales Jorge <jovivaspo@gmail.com>
2022-08-11 11:51:30 +0200

    fix carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,60 +1,80 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { fetchData } from '../services/fetchData'
-import './Carousel.css'
-import { BsChevronRight } from 'react-icons/bs'
-import { BsChevronLeft } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState, useRef } from "react";
+import { fetchData } from "../services/fetchData";
+import "./Carousel.css";
+import { BsChevronRight } from "react-icons/bs";
+import { BsChevronLeft } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Carousel = ({ title, url }) => {
-    const [content, setContent] = useState([])
-    const refCarousel = useRef()
+  const [content, setContent] = useState([]);
+  const refCarousel = useRef();
 
-    useEffect(() => {
-        const loadContent = () => {
-            fetchData(url).
-                then(res => {
-                    //console.log(res.data)
-                    setContent(res.data)
-                })
-        }
+  useEffect(() => {
+    const loadContent = () => {
+      fetchData(url).then((res) => {
+        //console.log(res.data)
+        setContent(res.data);
+      });
+    };
 
-        loadContent()
+    loadContent();
+  }, []);
 
-    }, [])
-
-    const handleClick = (e) => {
-        console.log(refCarousel.current.clientWidth)
-        if (e.target.className === "left-icon" || e.target.parentNode.className === "left-icon") {
-            console.log("left")
-            refCarousel.current.scrollRight += 627;
-            return false
-
-        }
-
-        if (e.target.className === "right-icon" || e.target.parentNode.className === "right-icon") {
-            console.log("right")
-            refCarousel.current.scrollLeft += 627;
-            return false
-        }
+  const handleClick = (e) => {
+    console.log(refCarousel.current.clientWidth);
+    if (
+      e.target.className === "left-icon" ||
+      e.target.parentNode.className === "left-icon"
+    ) {
+      console.log("left");
+      refCarousel.current.scrollLeft -= 627;
+      return false;
     }
 
+    if (
+      e.target.className === "right-icon" ||
+      e.target.parentNode.className === "right-icon"
+    ) {
+      console.log("right");
+      refCarousel.current.scrollLeft += 627;
+      return false;
+    }
+  };
 
-    return (
-        <div className='carousel-container'>
-            <h3 className='carousel-title'>{title}</h3>
-            <div className="carousel">
-                <div className='left-icon' onClick={handleClick}> <BsChevronLeft size={24} color="#fff" /></div>
-                <div className='right-icon' onClick={handleClick}> <BsChevronRight size={24} color="#fff" /></div>
-                {content && <div className='content-container' ref={refCarousel}>
-                    {content.map((el, index) => {
-                        return <Link to={`/gif-center/${el.type}/${el.slug}`} key={index} >
-                             <img loading="lazy" className="img" src={el.images.fixed_height_small.webp || el.images.fixed_height_small.url} alt={el.title} /></Link>
-                    })}
-                </div>
-                }
-            </div>
+  return (
+    <div className="carousel-container">
+      <h3 className="carousel-title">{title}</h3>
+      <div className="carousel">
+        <div className="left-icon" onClick={handleClick}>
+          {" "}
+          <BsChevronLeft size={24} color="#fff" />
         </div>
-    )
-}
+        <div className="right-icon" onClick={handleClick}>
+          {" "}
+          <BsChevronRight size={24} color="#fff" />
+        </div>
+        {content && (
+          <div className="content-container" ref={refCarousel}>
+            {content.map((el, index) => {
+              return (
+                <Link to={`/gif-center/${el.type}/${el.slug}`} key={index}>
+                  <img
+                    loading="lazy"
+                    className="img"
+                    src={
+                      el.images.fixed_height_small.webp ||
+                      el.images.fixed_height_small.url
+                    }
+                    alt={el.title}
+                  />
+                </Link>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Carousel
+export default Carousel;
